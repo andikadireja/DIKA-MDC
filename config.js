c332308a09f76378077a2ac46bd6c1d91a9f34b8
YT-DANIXD <117894945+YT-DANIXD@users.noreply.github.com>
2022-11-11 09:21:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE DANIXD'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281251549899','6281251549899','6281251549899']
+global.premium = ['6281251549899','6281348106274']
+global.packname = 'The Dani Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
